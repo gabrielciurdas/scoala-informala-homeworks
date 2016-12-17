984d53db45c816b712f9120109d3bd85f7381a7c
Gabriel Ciurdas\Gabi <gabrielciurdas@gmail.com>
2016-12-17 13:30:06 +0200

    Created CompanyManagementSoftware main class

---
+++ java-2016/homework-6/src/ro/siit/java5/homework6/CompanyManagementSoftware.java
@@ -0,0 +1,20 @@
+package ro.siit.java5.homework6;
+/**
+ * 
+ */
+
+/**
+ * @author Gabi
+ *
+ */
+public class CompanyManagementSoftware {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
