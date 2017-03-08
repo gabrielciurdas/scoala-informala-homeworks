d7d9ad39a3b60c1ae4cbd2119043a7d5f41ed0cb
Gabriel Ciurdas\Gabi <gabrielciurdas@gmail.com>
2017-03-08 13:20:23 +0200

    Project updated. Threads print the result after the packages are delivered.

--- java-2016/finalProject/src/ro/siit/java5/DeliveryThread.java
+++ java-2016/finalProject/src/ro/siit/java5/DeliveryThread.java
@@ -4,47 +4,58 @@ import java.util.List;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
-public class DeliveryThread extends Thread{
+public class DeliveryThread extends Thread {
 	private double merchandiseValue = 0;
 	private double profit = 0;
 	private int distanceToTarget = 0;
 	private List<Package> packs;
-	
+
 	public DeliveryThread(List<Package> packs, int distanceToTarget) {
 		this.distanceToTarget = distanceToTarget;
-		calculateMerchandiseValue(packs);
-		calculateTotalProfit(packs);
+		this.packs = packs;
 	}
-	
+
 	public void run() {
+		System.out.println(packs.get(0).getTargetLocation().name() + " package delivery started");
+		try {
+			TimeUnit.SECONDS.sleep(distanceToTarget);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 		calculateMerchandiseValue(packs);
 		calculateTotalProfit(packs);
+		System.out.println(toString());
 	}
-	
 
 	private void calculateTotalProfit(List<Package> packs) {
 		ProfitCalculator pc = new ProfitCalculator();
-		for(Package p: packs) {
+		for (Package p : packs) {
 			profit += pc.calculateProfit(p);
 		}
 	}
 
 	private void calculateMerchandiseValue(List<Package> packs) {
-		for(Package p: packs) {
+		for (Package p : packs) {
 			merchandiseValue += p.getMerchandiseValue();
 		}
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see java.lang.Object#toString()
 	 */
 	@Override
 	public String toString() {
-		return "DeliveryThread [merchandiseValue=" + merchandiseValue + ", profit=" + profit + "]";
+		return packs.get(0).getTargetLocation().name() + " DeliveryThread [merchandiseValue=" + merchandiseValue
+				+ ", profit=" + profit + "]" + " completed";
 	}
-	
+
 	public int getDistanceToTarget() {
 		return distanceToTarget;
 	}
-	
+
+	public List<Package> getPacks() {
+		return packs;
+	}
 }
